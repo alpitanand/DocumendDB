91c3b4b98089e27e42e3cd5cd3805a146d2295b1
Alpit_anand <alpit.anand@convergytics.net>
2019-08-06 15:14:55 +0530

    Db string

--- Database/Database.config.js
+++ Database/Database.config.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 mongoose.Promise = global.Promise
 //var ca = [fs.readFileSync(__dirname+'/rds-combined-ca-bundle.pem')];
 
-const url = 'mongodb://Sample:12345678@docdb-2019-07-30-08-45-53.cluster-czognfvnmvja.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0'
+const url = 'mongodb://Sample:<12345678>@docdb-2019-07-30-08-45-53.cluster-czognfvnmvja.us-east-1.docdb.amazonaws.com:27017'
 
 //const url = 'mongodb://localhost:27017/TestDocument'
 
